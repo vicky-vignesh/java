748880640489c8df112a9af03123d81378f85855
vicky-vignesh <42034715+vicky-vignesh@users.noreply.github.com>
2019-06-08 15:17:30 +0530

    Create CountFactorValue.java

---
+++ CountFactorValue.java
@@ -0,0 +1,42 @@
+package logic3;
+import java.util.Scanner;
+public class CountFactorValue {
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		    System.out.println("Enter the values:");
+			Scanner scan = new Scanner(System.in);
+			int num = scan.nextInt();
+			int ps [] = new int[num/2];
+			for(int i=0;i<ps.length;i++) {
+				ps[i]=i*i;
+			}
+
+			int arr [] = new int[num+1];
+			int count = 0;
+			for(int i=1;i<num-1;i++) {
+				if(num%i ==0) {
+					arr[count]=i;
+					count++;
+				}
+			}
+			int narr[]=new int[count];
+			for (int i = 0; i < count; i++) {
+				narr[i]=arr[i];
+				for(int j=0;j<ps.length;j++) {
+					if(narr[i]==ps[j]) {
+						narr[i]=0;
+					}
+				}
+			}
+			count=0;
+			for (int i = 0; i < narr.length; i++) {
+				if(narr[i]!=0) {
+					count++;
+				}
+			}
+			System.out.println(count);
+			
+		}
+
+	}
